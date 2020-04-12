6dc6172d01d1e49cb4e48bfda11bff534734f8cc
Meddaria <meddaria@gmail.com>
2020-04-12 03:08:12 +0300

    Updates

---
+++ precache-manifest.6597f24484d205e20b440673e052cc00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd408101116481407cce9b60339fc0c4",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "56d71975f9f7a9ebb15d",
+    "url": "/kanban/static/js/2.2494d628.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kanban/static/js/2.2494d628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01bd19d3dc47c5e612ed",
+    "url": "/kanban/static/js/main.186dfbfe.chunk.js"
+  },
+  {
+    "revision": "2bccd729a1062733c98b",
+    "url": "/kanban/static/js/runtime-main.b01b94cd.js"
+  }
+]);
